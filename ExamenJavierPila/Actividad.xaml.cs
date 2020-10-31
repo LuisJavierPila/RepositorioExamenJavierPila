1afe2fffb4d41cf67a9e2e08779e07acfbb35572
LuisJavierPila <73727120+LuisJavierPila@users.noreply.github.com>
2020-10-31 00:23:32 -0500

    Add files via upload

---
+++ ExamenJavierPila/Actividad.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenJavierPila
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Actividad : ContentPage
+    {
+       
+      public Actividad()
+        {
+
+        }
+        
+        public Actividad(string name, string mon, string men)
+
+        {
+            InitializeComponent();
+            string nombre = txtNombre.Text;
+            txtNombre.Text = nombre + name;
+
+            string monto = txtMonto.Text;
+            txtMonto.Text = monto + mon;
+
+            string mensual = txtMensual.Text;
+            txtMensual.Text = mensual + men;
+        }
+
+        
+    }
+    
+}
